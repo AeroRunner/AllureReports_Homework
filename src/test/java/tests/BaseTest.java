253b95c1c3bbbb9bf1c7761db992bcde1ea1b9af
Dmitrii Elizarov <airplay1x6@gmail.com>
2024-01-16 10:22:06 +0300

    update BaseTest Class (+AllureRepoerts)

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,12 +1,19 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
 
+import static io.qameta.allure.Allure.step;
+
 public class BaseTest {
+    public static String size = "1920x1080";
+    public static String loadStr = "eager";
     @BeforeAll
     public static void testsSetUP(){
-        Configuration.browserSize = "1920x1080";
-        Configuration.pageLoadStrategy = "eager";
+        SelenideLogger.addListener("allure",new AllureSelenide());
+        step("SetUp BrowserSize = " +size, () ->{Configuration.browserSize = size;});
+        step("SetUP pageLoadStrategy = "+loadStr, () -> { Configuration.pageLoadStrategy = loadStr;});
     }
 }
